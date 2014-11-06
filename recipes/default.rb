243569647961aae6759ed328480258d156fbf006
okisanjp <okisan.jp@gmail.com>
2014-11-06 11:48:18 +0900

    first commit

---
+++ recipes/default.rb
@@ -0,0 +1,31 @@
+#
+# Cookbook Name:: chef-recipes-devopstools
+# Recipe:: default
+#
+# Copyright 2014, YOUR_COMPANY_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
+
+# yumを最新に
+bash 'yum update' do
+	user "root"
+	code <<-EOS
+		yum -y update
+		yum -y upgrade
+	EOS
+end
+
+# 入れておきたいツール
+%w{
+	htop vim-enhanced git-all
+}.each do |pkg|
+	package pkg do
+	  action :upgrade
+	end
+end
+
+# yum-cronを入れておく
+service "yum-cron" do
+  action [ :enable, :start ]
+end
